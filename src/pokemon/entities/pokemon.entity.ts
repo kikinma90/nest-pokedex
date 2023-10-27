e7c1f7b1234047843c10d2fe6492b4f261bceff2
kikinma90 <juan_manuel90@msn.com>
2023-10-27 16:02:21 +0200

    Primera parte del proyecto

---
+++ src/pokemon/entities/pokemon.entity.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+//Las entidades hacen una referencia a como queremos grabar en la base de datos. En mongodb se llama coleccion.
+
+@Schema()
+export class Pokemon extends Document{
+
+    //id // Mongo ya te lo proporciona
+    // Utilizamos decoradores para indicar que estas propiedades son unicas y tienen indice para poder realizar busquedas en la db de mongo
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    name: string;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    no: string;
+
+}
+
+// Exportamos el esquema para poder utilizarlo en otros archivos
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
